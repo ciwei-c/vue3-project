fa37071a1b5ad417b8febb08803c0168819cf54b
ciwei-c <ciwei_c@163.com>
2023-06-15 15:43:29 +0800

    resolve feature/test04

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a-button type="primary">Primary Button feature/test03</a-button>
+    <a-button type="primary">Primary Button feature/test04</a-button>
   </div>
 </template>
 <script></script>
